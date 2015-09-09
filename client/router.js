439e154d9245c031715f4ead926e744b85df404e
Spacemixup <xchrislee@gmail.com>
2015-09-08 18:22:12 -0700

    Add posting ability into home.

---
+++ client/router.js
@@ -0,0 +1,17 @@
+Router.configure({
+  layoutTemplate: 'layout'
+  // ,
+  // notFoundTemplate: 'notFound'
+  // ,
+  // loadingTemplate: 'loading'
+});
+
+
+Router.route('/', {name: 'home'})
+
+// Router.map(function() {
+//   // this.route('splash', {path: '/'})
+//  //  this.route('messages');
+// 	// this.route('events');
+// 	this.route('home');
+// });
